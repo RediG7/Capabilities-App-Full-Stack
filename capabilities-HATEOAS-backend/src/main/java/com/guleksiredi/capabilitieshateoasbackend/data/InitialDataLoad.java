6295318bbf57efe8b7e25011ad4c167ff6c9173a
RediG7 <guleksiredi7@gmail.com>
2021-12-11 15:36:41 +0100

    Initial Data Load

---
+++ capabilities-HATEOAS-backend/src/main/java/com/guleksiredi/capabilitieshateoasbackend/data/InitialDataLoad.java
@@ -0,0 +1,20 @@
+package com.guleksiredi.capabilitieshateoasbackend.data;
+
+import com.guleksiredi.capabilitieshateoasbackend.domain.Capability;
+import com.guleksiredi.capabilitieshateoasbackend.repositories.CapabilityRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class InitialDataLoad {
+
+    @Bean
+    CommandLineRunner LoadDB(CapabilityRepository capabilityRepository) {
+        return args -> {
+            capabilityRepository.save(new Capability("Java", 100, 500));
+            capabilityRepository.save(new Capability("ReactJS", 70, 20));
+            capabilityRepository.save(new Capability("Python", 200, 100));
+        };
+    }
+}
