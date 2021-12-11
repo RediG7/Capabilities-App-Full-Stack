09342616266a1f16552bb12567533545d415a613
RediG7 <guleksiredi7@gmail.com>
2021-12-11 15:52:51 +0100

    Service Layer

---
+++ capabilities-HATEOAS-backend/src/main/java/com/guleksiredi/capabilitieshateoasbackend/services/CapabilityService.java
@@ -0,0 +1,30 @@
+package com.guleksiredi.capabilitieshateoasbackend.services;
+
+import com.guleksiredi.capabilitieshateoasbackend.domain.Capability;
+import com.guleksiredi.capabilitieshateoasbackend.exceptions.CapabilityException;
+import com.guleksiredi.capabilitieshateoasbackend.repositories.CapabilityRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CapabilityService {
+
+    private final CapabilityRepository capabilityRepository;
+
+    public CapabilityService(CapabilityRepository capabilityRepository) {
+        this.capabilityRepository = capabilityRepository;
+    }
+
+    public List<Capability> getAllCapabilities() {
+        return capabilityRepository.findAll();
+    }
+
+    public Capability findCapabilityById(Long id) {
+
+        return capabilityRepository.findById(id).orElseThrow(
+                () -> new CapabilityException("Capability with ID: " + id + " not found")
+        );
+    }
+
+}
