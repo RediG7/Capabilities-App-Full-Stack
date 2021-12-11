af3b2560539e1366c35d3a1a26bfc9ceb4b28cac
RediG7 <guleksiredi7@gmail.com>
2021-12-11 15:52:55 +0100

    Custom Exceptions

---
+++ capabilities-HATEOAS-backend/src/main/java/com/guleksiredi/capabilitieshateoasbackend/exceptions/CapabilityException.java
@@ -0,0 +1,7 @@
+package com.guleksiredi.capabilitieshateoasbackend.exceptions;
+
+public class CapabilityException extends RuntimeException {
+    public CapabilityException(String message) {
+        super(message);
+    }
+}
---
+++ capabilities-HATEOAS-backend/src/main/java/com/guleksiredi/capabilitieshateoasbackend/exceptions/CapabilityNotFoundResponse.java
@@ -0,0 +1,10 @@
+package com.guleksiredi.capabilitieshateoasbackend.exceptions;
+
+public class CapabilityNotFoundResponse {
+
+    private String capabilityNotFound;
+
+    public CapabilityNotFoundResponse(String capabilityNotFound) {
+        this.capabilityNotFound = capabilityNotFound;
+    }
+}
