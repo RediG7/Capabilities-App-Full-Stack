1dfaf66ed1cfba50bc94544c1a6dcc86810d4df0
RediG7 <guleksiredi7@gmail.com>
2021-12-11 13:20:52 +0100

    Domain And Repository

---
+++ src/main/java/com/guleksiredi/capabilitieshateoasbackend/repositories/CapabilityRepository.java
@@ -0,0 +1,9 @@
+package com.guleksiredi.capabilitieshateoasbackend.repositories;
+
+import com.guleksiredi.capabilitieshateoasbackend.domain.Capability;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CapabilityRepository extends JpaRepository<Capability, Long> {
+
+
+}
