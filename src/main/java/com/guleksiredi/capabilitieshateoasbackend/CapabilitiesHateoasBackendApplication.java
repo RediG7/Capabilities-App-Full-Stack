4b473988f576fe289eb51f2a38e29998c27fef96
RediG7 <guleksiredi7@gmail.com>
2021-12-11 13:04:45 +0100

    Initial Commit

---
+++ src/main/java/com/guleksiredi/capabilitieshateoasbackend/CapabilitiesHateoasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.guleksiredi.capabilitieshateoasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapabilitiesHateoasBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapabilitiesHateoasBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guleksiredi/capabilitieshateoasbackend/CapabilitiesHateoasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guleksiredi.capabilitieshateoasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapabilitiesHateoasBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
