a91f09522a3525b404159f49a2f2f1dd8558c2fa
RediG7 <guleksiredi7@gmail.com>
2022-01-17 19:08:57 +0100

    Implement RepresentationModelAssembler

--- capabilities-HATEOAS-backend/src/main/java/com/guleksiredi/capabilitieshateoasbackend/resources/CapabilityController.java
+++ capabilities-HATEOAS-backend/src/main/java/com/guleksiredi/capabilitieshateoasbackend/resources/CapabilityController.java
@@ -1,16 +1,17 @@
 package com.guleksiredi.capabilitieshateoasbackend.resources;
 
+import com.guleksiredi.capabilitieshateoasbackend.assembler.CapabilityRepresentationModelAssembler;
 import com.guleksiredi.capabilitieshateoasbackend.domain.Capability;
 import com.guleksiredi.capabilitieshateoasbackend.services.CapabilityService;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,33 +26,45 @@ public class CapabilityController {
 
     private final CapabilityService capabilityService;
 
-    public CapabilityController(CapabilityService capabilityService) {
+    private final CapabilityRepresentationModelAssembler assembler;
+
+    public CapabilityController(CapabilityService capabilityService, CapabilityRepresentationModelAssembler assembler) {
         this.capabilityService = capabilityService;
+        this.assembler = assembler;
     }
 
     @GetMapping
-    CollectionModel<EntityModel<Capability>> getAllCapabilities() {
+    public CollectionModel<EntityModel<Capability>> getAllCapabilities() {
 
-        List<EntityModel<Capability>> capabilities =
-                capabilityService.getAllCapabilities().stream()
-                        .map(capability -> EntityModel.of(capability,
-                                linkTo(methodOn(CapabilityController.class).getCapability(capability.getId())).withRel("getThisCapability"),
-                                linkTo(methodOn(CapabilityController.class).getAllCapabilities()).withRel("getAllCapabilities")
-                        ))
-                        .collect(Collectors.toList());
+//        List<EntityModel<Capability>> capabilities =
+//                capabilityService.getAllCapabilities().stream()
+//                        .map(capability -> EntityModel.of(capability,
+//                                linkTo(methodOn(CapabilityController.class).getCapability(capability.getId())).withRel("getThisCapability"),
+//                                linkTo(methodOn(CapabilityController.class).getAllCapabilities()).withRel("getAllCapabilities")
+//                        ))
+//                        .collect(Collectors.toList());
+//
+//        return CollectionModel.of(capabilities, linkTo(methodOn(CapabilityController.class).getAllCapabilities()).withSelfRel());
 
-        return CollectionModel.of(capabilities, linkTo(methodOn(CapabilityController.class).getAllCapabilities()).withSelfRel());
-
+        return CollectionModel.of(capabilityService.getAllCapabilities().stream()
+                        .map(assembler::toModel).collect(Collectors.toList()),
+                Link.of("http://localhost:8080/dashboard").withRel("createCapability")
+        );
+        // map(capability -> assembler.toModel(capability)) converted to .map(assembler::toModel)
+        // Lambda -> To Method Reference
     }
 
     @GetMapping("/{id}")
-    public EntityModel<Capability> getCapability(@PathVariable Long id) {
+    public EntityModel<?> getCapability(@PathVariable Long id) {
         // Resource ->  Generic Container, contains data but also a set or collection of links
-        Capability capability = capabilityService.findCapabilityById(id);
+//        Capability capability = capabilityService.findCapabilityById(id);
+//
+//        return EntityModel.of(capability,
+//                linkTo(methodOn(CapabilityController.class).getCapability(id)).withRel("Current Link"),
+//                linkTo(methodOn(CapabilityController.class).getAllCapabilities()).withRel("getAllCapabilities"));
 
-        return EntityModel.of(capability,
-                linkTo(methodOn(CapabilityController.class).getCapability(id)).withRel("Current Link"),
-                linkTo(methodOn(CapabilityController.class).getAllCapabilities()).withRel("getAllCapabilities"));
+        return EntityModel.of(assembler.toModel(capabilityService.findCapabilityById(id)));
+        // return (assembler.toModel(capabilityService.findCapabilityById(id))); // can be shorter
     }
 
     @PostMapping
@@ -62,23 +75,24 @@ public class CapabilityController {
 
         if (result.hasErrors()) return capabilityService.errorMap(result);
 
-
-        Capability newCapability = capabilityService.saveCapability(capability);
-
-        return EntityModel.of(newCapability,
-                linkTo(methodOn(CapabilityController.class).getCapability(newCapability.getId())).withRel("Current Link"),
-                linkTo(methodOn(CapabilityController.class).getAllCapabilities()).withRel("getAllCapabilities"));
+//        Capability newCapability = capabilityService.saveCapability(capability);
+//
+//        return EntityModel.of(newCapability,
+//                linkTo(methodOn(CapabilityController.class).getCapability(newCapability.getId())).withRel("Current Link"),
+//                linkTo(methodOn(CapabilityController.class).getAllCapabilities()).withRel("getAllCapabilities"));
+        return EntityModel.of(assembler.toModel(capabilityService.saveCapability(capability)));
     }
 
     @PutMapping("/{id}")
     public Object updateCapability(@PathVariable Long id, @Valid @RequestBody Capability capability, BindingResult result) {
         if (result.hasErrors()) return capabilityService.errorMap(result);
 
-        Capability capabilityToUpdate = capabilityService.updateCapability(id, capability);
-
-        return EntityModel.of(capabilityToUpdate,
-                linkTo(methodOn(CapabilityController.class).getCapability(capabilityToUpdate.getId())).withRel("Current Link (Capability)"),
-                linkTo(methodOn(CapabilityController.class).getAllCapabilities()).withRel("getAllCapabilities"));
+//        Capability capabilityToUpdate = capabilityService.updateCapability(id, capability);
+//
+//        return EntityModel.of(capabilityToUpdate,
+//                linkTo(methodOn(CapabilityController.class).getCapability(capabilityToUpdate.getId())).withRel("Current Link (Capability)"),
+//                linkTo(methodOn(CapabilityController.class).getAllCapabilities()).withRel("getAllCapabilities"));
+        return EntityModel.of(assembler.toModel(capabilityService.updateCapability(id, capability)));
     }
 
     @DeleteMapping("/{id}")
