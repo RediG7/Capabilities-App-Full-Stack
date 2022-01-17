9844a0b92328ad18981f955ac00cc2f9c5a69e4c
RediG7 <guleksiredi7@gmail.com>
2022-01-17 19:08:08 +0100

    Representation Model Assembler

---
+++ capabilities-HATEOAS-backend/src/main/java/com/guleksiredi/capabilitieshateoasbackend/assembler/CapabilityRepresentationModelAssembler.java
@@ -0,0 +1,27 @@
+package com.guleksiredi.capabilitieshateoasbackend.assembler;
+
+import com.guleksiredi.capabilitieshateoasbackend.domain.Capability;
+import com.guleksiredi.capabilitieshateoasbackend.resources.CapabilityController;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+// from non resource obj -> to resource obj
+@Component
+public class CapabilityRepresentationModelAssembler implements RepresentationModelAssembler<Capability, EntityModel<Capability>> {
+    @Override
+    public EntityModel<Capability> toModel(Capability entity) {
+        return EntityModel.of(entity,
+                linkTo(methodOn(CapabilityController.class).getCapability(entity.getId())).withRel("getThisCapability"),
+                linkTo(methodOn(CapabilityController.class).deleteCapability(entity.getId())).withRel("deleteThisCapability"),
+                linkTo(methodOn(CapabilityController.class).getAllCapabilities()).withRel("getAllCapabilities"),
+                Link.of("http://localhost:8080/dashboard/" + entity.getId()).withRel("updateThisCapability")
+        );
+    }
+
+}
