b63515726cbfd5b6ccd3a238e3a4bfbc4c4fd765
RediG7 <guleksiredi7@gmail.com>
2022-01-21 23:11:49 +0100

    Get Capabilities Part 2 (mapStateToProps & dynamic render)

--- capabilities-react-frontend/src/components/CapabilityTool/Capability.js
+++ capabilities-react-frontend/src/components/CapabilityTool/Capability.js
@@ -1,29 +1,37 @@
-  import React, { Component } from "react";
+import React, { Component } from "react";
 
-  export class Capability extends Component {
-    render() {
-      return (
-        <div className="card card-body border-primary mb-3">
-          <h4 className="text-primary">
-            Sample Capability
-            <i className="fas fa-user-edit ml-2" style={{ color: "blue" }} />
-            <i className="fas fa-user-times ml-2" style={{ color: "red" }} />
-          </h4>
+export class Capability extends Component {
+  render() {
+    const {
+      id,
+      numOfAvailableDevelopers,
+      numOfDevelopers,
+      techStack,
+      _links,
+    } = this.props.capability;
 
-          <ul className="list-group">
-            <li className="list-group-item bg-light text-primary">
-              Technology Stack: Sample Tech Stack
-            </li>
-            <li className="list-group-item bg-light  text-primary">
-              Total Developers in Capability: 200
-            </li>
-            <li className="list-group-item bg-light  text-primary">
-              Available developers for hire: 20
-            </li>
-          </ul>
-        </div>
-      );
-    }
+    return (
+      <div className="card card-body border-primary mb-3">
+        <h4 className="text-primary">
+          {techStack} ... {_links.getThisCapability.href}
+          <i className="fas fa-user-edit ml-2" style={{ color: "blue" }} />
+          <i className="fas fa-user-times ml-2" style={{ color: "red" }} />
+        </h4>
+
+        <ul className="list-group">
+          <li className="list-group-item bg-light text-primary">
+            Technology Stack: {techStack}
+          </li>
+          <li className="list-group-item bg-light  text-primary">
+            Total Developers in Capability: {numOfDevelopers}
+          </li>
+          <li className="list-group-item bg-light  text-primary">
+            Available developers for hire: {numOfAvailableDevelopers}
+          </li>
+        </ul>
+      </div>
+    );
   }
+}
 
-  export default Capability;
+export default Capability;
