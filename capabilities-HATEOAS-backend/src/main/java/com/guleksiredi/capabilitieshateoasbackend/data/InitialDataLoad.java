3ee95ef4d2920f7191e332c01a573dabee9a0937
RediG7 <guleksiredi7@gmail.com>
2022-02-13 17:56:32 +0100

    Disable after first run

--- capabilities-HATEOAS-backend/src/main/java/com/guleksiredi/capabilitieshateoasbackend/data/InitialDataLoad.java
+++ capabilities-HATEOAS-backend/src/main/java/com/guleksiredi/capabilitieshateoasbackend/data/InitialDataLoad.java
@@ -9,12 +9,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class InitialDataLoad {
 
-    @Bean
-    CommandLineRunner LoadDB(CapabilityRepository capabilityRepository) {
-        return args -> {
-            capabilityRepository.save(new Capability("Java", 100, 500));
-            capabilityRepository.save(new Capability("ReactJS", 70, 20));
-            capabilityRepository.save(new Capability("Python", 200, 100));
-        };
-    }
+//    @Bean
+//    CommandLineRunner LoadDB(CapabilityRepository capabilityRepository) {
+//        return args -> {
+//            capabilityRepository.save(new Capability("Java", 100, 500));
+//            capabilityRepository.save(new Capability("ReactJS", 70, 20));
+//            capabilityRepository.save(new Capability("Python", 200, 100));
+//        };
+//    }
 }
